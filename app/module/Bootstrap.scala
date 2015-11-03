486324c6e02f9413ddc5a6c24012cbc541968ddf
Fernando Benjamin <fernando.benjamin@zalando.de>
2015-11-03 20:56:36 +0100

    Synch timeouts must be shorter than time between consequent runs. #94 (gh).

--- app/module/Bootstrap.scala
+++ app/module/Bootstrap.scala
@@ -1,20 +1,14 @@
 package module
 
-import javax.inject._
-import play.api.Application
-import play.api.Logger
-import scala.concurrent.duration.DurationInt
-import play.api.libs.concurrent.Execution.Implicits.defaultContext
-import dao.CommitRepository
 import scala.concurrent.Await
-import dao.RepoRepository
-import dao.AuthorRepository
-import model.Author
-import model.Author
-import model.Link
+import scala.concurrent.duration.DurationInt
+
 import akka.actor.ActorSystem
-import service.ImportCommit
-import service.ImportRepository
+import dao.{ CommitRepository, RepoRepository }
+import javax.inject.{ Inject, Singleton }
+import play.api.Logger
+import play.api.libs.concurrent.Execution.Implicits.defaultContext
+import service.{ ImportCommit, ImportRepository }
 /**
  * @author fbenjamin
  */
@@ -27,7 +21,6 @@ class BootstrapImpl @Inject() (actorSystem: ActorSystem,
                                repoImporter: ImportRepository,
                                commitImporter: ImportCommit,
                                repoRepo: RepoRepository, //
-                               authorRepo: AuthorRepository, //
                                commitRepo: CommitRepository) extends Bootstrap {
   val logger: Logger = Logger { this.getClass }
 
@@ -35,24 +28,22 @@ class BootstrapImpl @Inject() (actorSystem: ActorSystem,
 
   def scheduleSyncAppsJob() = actorSystem.scheduler.schedule(12.seconds, 120.seconds) {
     logger.info("Started the synch job for synchronizing AppInfos(SCM-URL's) from KIO")
-    Await.result(repoImporter.syncApps(), 120.seconds)
+    Await.result(repoImporter.syncApps(), 180.seconds)
     logger.info("Finished the synch job for synchronizing AppInfos(SCM-URL's) from KIO")
   }
 
-  def scheduleSynchCommitsJobs() = actorSystem.scheduler.schedule(12.seconds, 300.seconds) {
+  def scheduleSynchCommitsJobs() = actorSystem.scheduler.schedule(500.seconds, 20.seconds) {
     logger.info("Started the job for synchronizing Commits from the SCM's")
     Await.result(commitImporter.synchCommits(), 300.seconds)
   }
 
-  def scheduleDatabaseBootstrap() =
-    actorSystem.scheduler.scheduleOnce(7.seconds) {
-      logger.info("Started bootstrapping initial database")
-      for {
-        authorsResult <- authorRepo.initializeDatabase
-        repoResult <- repoRepo.initializeDatabase
-        commitsResult <- commitRepo.initializeDatabase
-      } yield (authorsResult)
-    }
+  def scheduleDatabaseBootstrap() = actorSystem.scheduler.scheduleOnce(7.seconds) {
+    logger.info("Started bootstrapping initial database")
+    for {
+      repoResult <- repoRepo.initializeDatabase
+      commitsResult <- commitRepo.initializeDatabase
+    } yield (commitsResult)
+  }
 
   def setup() = {
     scheduleSyncAppsJob
