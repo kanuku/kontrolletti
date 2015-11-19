bf16d06e9154e5feb1b34fded5e6c00ddb2ef7fc
Fernando Benjamin <fernando.benjamin@zalando.de>
2015-11-19 13:13:54 +0100

    Moving imports for sake of maintenance. #86

--- app/dao/Tables.scala
+++ app/dao/Tables.scala
@@ -14,14 +14,13 @@ import play.api.libs.concurrent.Execution.Implicits.defaultContext
 import play.api.libs.json.JsValue
 import utility.Transformer
 import utility.GeneralHelper
+import Transformer._
+import dao.KontrollettiPostgresDriver.api._
+
+import KontrollettiToJsonParser.linkWriter
+import KontrollettiToModelParser.linkReader
 
 object Tables extends GeneralHelper { self: HasDatabaseConfigProvider[KontrollettiPostgresDriver] =>
-
-  import Transformer._
-  import dao.KontrollettiPostgresDriver.api._
-
-  import KontrollettiToJsonParser.linkWriter
-  import KontrollettiToModelParser.linkReader
 
   private val schema = Some("kont_data")
 
